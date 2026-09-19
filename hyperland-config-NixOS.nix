612a15d7f6d1d1370efc7a31fa6ce0426784a39e
XMV22 <cosmic.guy42@gmail.com>
2026-09-19 23:30:10 +0200

    Enable zramSwap to avoid system freezes

--- hyperland-config-NixOS.nix
+++ hyperland-config-NixOS.nix
@@ -46,6 +46,7 @@ in
   };
 
   swapDevices = [ ];
+  zramSwap.enable = true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
